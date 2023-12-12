6815f883c1b7491615ca66639fd23e22d45927a5
Rick South <rick@natamus.com>
2023-12-12 09:38:29 +0100

    Move Boats source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/moveboats/neoforge/events/NeoForgeBoatEvent.java
@@ -0,0 +1,33 @@
+package com.natamus.moveboats.neoforge.events;
+
+import com.natamus.moveboats.events.BoatEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeBoatEvent {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || !e.phase.equals(TickEvent.Phase.START)) {
+			return;
+		}
+
+		BoatEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+
+	@SubscribeEvent
+	public static void onBoatClick(PlayerInteractEvent.EntityInteract e) {
+		if (BoatEvent.onBoatClick(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.SUCCESS)) {
+			e.setCanceled(true);
+		}
+	}
+}
