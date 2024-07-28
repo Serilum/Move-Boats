d921a6018ebeb6e2ad633c0ca417d9c2a318f56b
Rick South <rick@natamus.com>
2024-07-28 07:24:51 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/moveboats/util/Reference.java
+++ Common/src/main/java/com/natamus/moveboats/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.moveboats.util;
 public class Reference {
 	public static final String MOD_ID = "moveboats";
 	public static final String NAME = "Move Boats";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
